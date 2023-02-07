40c94fbd461f3c22bab2207aa19c1e9c0696d77b
Viktor <vitya.malahov.95@gmail.com>
2023-02-07 16:37:06 +0200

    lesson-2

---
+++ JUnit5-maven-starter/src/test/java/com/victor/junit/service/UserServiceTest.java
@@ -0,0 +1,12 @@
+package com.victor.junit.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserServiceTest {
+    @Test
+    public void test() {
+        assertTrue(false);
+    }
+}
