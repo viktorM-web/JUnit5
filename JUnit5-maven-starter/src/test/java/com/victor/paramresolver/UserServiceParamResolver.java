239ad6418ecb17487fb69812074c62f7d512c450
Viktor <vitya.malahov.95@gmail.com>
2023-02-09 10:03:21 +0200

    lesson-11

---
+++ JUnit5-maven-starter/src/test/java/com/victor/paramresolver/UserServiceParamResolver.java
@@ -0,0 +1,28 @@
+package com.victor.paramresolver;
+
+import com.victor.service.UserService;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+
+import static org.junit.jupiter.api.extension.ExtensionContext.Namespace;
+
+public class UserServiceParamResolver implements ParameterResolver {
+    @Override
+    public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        return parameterContext.getParameter().getType() == UserService.class;
+    }
+
+    @Override
+    public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+//        var store = extensionContext.getStore(Namespace.create(UserService.class));
+//        return store.getOrComputeIfAbsent(UserService.class, it -> new UserService());
+
+        var store = extensionContext.getStore(Namespace.create(extensionContext.getTestMethod()));
+        return store.getOrComputeIfAbsent(extensionContext.getTestMethod(), it -> new UserService());
+
+
+//        return  new UserService();
+    }
+}
